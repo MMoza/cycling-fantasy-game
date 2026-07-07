d0a35a9ba16989666381115cd36cbaf954846250
m.moza <miguelangelmozabarquilla@gmail.com>
2026-07-07 13:21:17 +0200

    fix: return ArrayBuffer from VAPID key parser for TS compat

--- resources/js/hooks/usePushNotifications.ts
+++ resources/js/hooks/usePushNotifications.ts
@@ -2,7 +2,7 @@ import { useEffect, useState, useCallback } from 'react';
 
 const VAPID_PUBLIC_KEY = import.meta.env.VITE_VAPID_PUBLIC_KEY;
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
     const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
     const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
     const rawData = window.atob(base64);
@@ -10,7 +10,7 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
     for (let i = 0; i < rawData.length; ++i) {
         outputArray[i] = rawData.charCodeAt(i);
     }
-    return outputArray;
+    return outputArray.buffer;
 }
 
 export function usePushNotifications() {
